a3c186b515f555635a2ae1cd13a11d2384b9bbb7
Beatriz da Silveira <beatriz1304@gmail.com>
2018-02-06 00:03:33 -0200

    Implements requests global configuration

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,7 +10,7 @@ class FullPost extends Component {
     componentDidUpdate() {
         if (this.props.id) {
             if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.id)
                 .then(response => {
                     this.setState({loadedPost: response.data});
                 });
@@ -19,7 +19,7 @@ class FullPost extends Component {
     }
 
     deletePostHandler =() => {
-        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.id)
             .then(response => {
                 console.log(response);
             });
