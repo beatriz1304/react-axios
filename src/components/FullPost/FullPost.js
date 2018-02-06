7708037bcf308af8fde7357bb051facd276df782
Beatriz da Silveira <beatriz1304@gmail.com>
2018-02-05 23:08:13 -0200

    Shows full post when a post card is clicked

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null,
+    }
+    componentDidUpdate() {
+        if (this.props.id) {
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then(response => {
+                    this.setState({loadedPost: response.data});
+                });
+            }
+        }
+    }
     render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if(this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...!</p>;
+        }
+        if(this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
                 </div>
-            </div>
 
-        );
+            );
+        }
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,20 +9,35 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-      posts: []
+      posts: [],
+      selectedPostId: null
     }
 
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-              this.setState({posts: response.data});
-                console.log(response);
+              const posts = response.data.slice(0, 4);
+              const updatedPosts = posts.map(post => {
+                  return {
+                      ...post,
+                      author: 'Bia'
+                  }
+              })
+              this.setState({posts: updatedPosts});
             });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title}/>
+            return <Post
+              key={post.id}
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)}/>
         });
 
         return (
@@ -31,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
