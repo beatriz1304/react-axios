4b3ff65198dabbddfbe201ee3aae88b52cf72a6a
Beatriz da Silveira <beatriz1304@gmail.com>
2018-02-07 22:40:42 -0200

    Adjusts project for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,64 +1,22 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
 
-    state = {
-      posts: [],
-      selectedPostId: null,
-      error: false
-    }
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-              const posts = response.data.slice(0, 4);
-              const updatedPosts = posts.map(post => {
-                  return {
-                      ...post,
-                      author: 'Bia'
-                  }
-              });
-              this.setState({posts: updatedPosts});
-            })
-            .catch(error => {
-              this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if(!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}/>
-                });
-        }
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                  <nav>
+                    <ul>
+                      <li><a href="/">Home</a></li>
+                      <li><a href="/new-post">New Post</a></li>
+                    </ul>
+                  </nav>
+                </header>
+                <Posts />
             </div>
         );
     }
