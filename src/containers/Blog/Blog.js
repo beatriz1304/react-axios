4d68d80691b7405062f1f1598eb39e6623f4f4bd
Beatriz da Silveira <beatriz1304@gmail.com>
2018-02-07 22:53:05 -0200

    Renders components for routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -16,7 +17,8 @@ class Blog extends Component {
                     </ul>
                   </nav>
                 </header>
-                <Posts />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>}/> */}
+                <Route path="/" exact component={Posts} />
             </div>
         );
     }
