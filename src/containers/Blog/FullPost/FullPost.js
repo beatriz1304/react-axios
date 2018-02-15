d46dac89780445204bb8986a433366e029362da6
Beatriz da Silveira <beatriz1304@gmail.com>
2018-02-14 22:08:28 -0200

    Opens FullPost component via route extraction id parameter

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -7,10 +7,11 @@ class FullPost extends Component {
     state = {
         loadedPost: null,
     }
-    componentDidUpdate() {
-        if (this.props.id) {
+    componentDidMount() {
+        console.log(this.props);
+        if (this.props.match.params.id) {
             if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.match.params.id)
                 .then(response => {
                     this.setState({loadedPost: response.data});
                 });
